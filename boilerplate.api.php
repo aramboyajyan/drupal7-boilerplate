c6fb834a10b62454e3bc66f24b0c769356f6d059
aramboyajyan <dev@aram.cz>
2016-07-08 14:02:43 +0200

    fixes #48

---
+++ boilerplate.api.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Boilerplate module.
+ *
+ * Created by: Topsitemakers
+ * http://www.topsitemakers.com/
+ */
+
+// Include instructions for integration with your custom module.
